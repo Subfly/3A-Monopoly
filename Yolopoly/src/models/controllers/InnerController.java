a5f9b88173ddd8e2ac192238cd03dc30f2496d3d
Etophiana <invokeer@gmail.com>
2020-11-27 03:16:12 +0300

    Middle Controller visually ready. Mode, theme and size buttons are functional. Press Single Player for Middle Controller.

--- Yolopoly/src/models/controllers/InnerController.java
+++ Yolopoly/src/models/controllers/InnerController.java
@@ -1,17 +1,6 @@
 package models.controllers;
 
-import javafx.stage.Stage;
-
 public class InnerController {
 
-    Stage primaryStage;
-
-    public InnerController() {
-        this.primaryStage = null;
-    }
-
-    public void setPrimaryStage(Stage primaryStage){
-        this.primaryStage = primaryStage;
-    }
 
 }
