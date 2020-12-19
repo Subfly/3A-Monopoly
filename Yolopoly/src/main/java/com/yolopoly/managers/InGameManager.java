e3706e4d0d26dfd738c4ea4d4d797bddf77414e9
ecem yelkanat <iremecemyelkanat@gmail.com>
2020-12-19 10:21:07 +0300

    some logs added

--- Yolopoly/src/main/java/com/yolopoly/managers/InGameManager.java
+++ Yolopoly/src/main/java/com/yolopoly/managers/InGameManager.java
@@ -1101,6 +1101,7 @@ public class InGameManager {
         Player player = players.get(currentPlayerId);
         if (player.isInJail()) {
             if (player.removeMoney(Constants.CURRENCY_NAMES[0], (int)(Bank.getJailPenalty() * multiplier))) {
+                addToLog("has got out of jail", player.getName());
                 player.setInJail(false);
                 player.resetInJailTurnCount();
                 return 1;
@@ -1185,6 +1186,7 @@ public class InGameManager {
         else {
             result = 2.0;
         }
+        addToLog("has a chance multiplier of " + result + "for the turn", players.get(currentPlayerId).getName());
         return result;
     }
 
